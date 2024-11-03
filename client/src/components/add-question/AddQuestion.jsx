c6e2a44f73ce0368d4ab5c76184dfd09aa9f62a7
kilvencodes <kilven.coder01@gmail.com>
2024-11-03 09:25:40 +0800

    Add Loading State for fetching questions

--- client/src/components/add-question/AddQuestion.jsx
+++ client/src/components/add-question/AddQuestion.jsx
@@ -38,11 +38,11 @@ export default function AddQuestion({ modalSetting, setModalSetting }) {
     searchParams.get('subject') || listOfSubjects?.subjects[0]?.id;
   const page = searchParams.get('page') || 1;
 
-  const { data: listOfQuestions, isPlaceholderData } = useGetAllQuestions(
-    subject,
-    Number(page - 1),
-    50
-  );
+  const {
+    data: listOfQuestions,
+    isPlaceholderData,
+    isLoading,
+  } = useGetAllQuestions(subject, Number(page - 1), 50);
 
   const onPageClick = (pageNumber) => {
     setSearchParams((prev) => {
@@ -133,53 +133,57 @@ export default function AddQuestion({ modalSetting, setModalSetting }) {
         </div>
       </CardHeader>
       <CardContent className='max-h-[600px] overflow-auto mx-4 mb-8'>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Question</TableHead>
-              <TableHead>Options</TableHead>
-              <TableHead>Subject</TableHead>
-              <TableHead>Action</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {listOfQuestions?.questions?.map((question, index) => (
-              <TableRow key={index}>
-                <TableCell className='font-medium w-[40%]'>
-                  {question.question}
-                </TableCell>
-                <TableCell className='font-medium w-[35%]'>
-                  {question.options.map((option, index) => (
-                    <p key={index} className='leading-4 py-0.5'>
-                      {`${index + 1}. ${option},`}
-                    </p>
-                  ))}
-                </TableCell>
-                <TableCell className='font-medium'>
-                  <p>{question.subject.title}</p>
-                  <Badge className='italic shadow-none'>
-                    Answer: {Number(question.answer)}
-                  </Badge>
-                </TableCell>
-                <TableCell className='font-medium  space-y-3 flex flex-col items-start'>
-                  <Button
-                    size='sm'
-                    onClick={() => handleOpenModal(question, false)}
-                  >
-                    <PenBox size={18} className='mr-1' /> Edit
-                  </Button>
-                  <Button
-                    size='sm'
-                    variant='destructive'
-                    onClick={() => handleOpenModal(question.id, true)}
-                  >
-                    <Trash size={18} className='mr-1' /> Delete
-                  </Button>
-                </TableCell>
+        {isLoading ? (
+          <p className='text-center my-2 italic'>Loading Questions...</p>
+        ) : (
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Question</TableHead>
+                <TableHead>Options</TableHead>
+                <TableHead>Subject</TableHead>
+                <TableHead>Action</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {listOfQuestions?.questions?.map((question, index) => (
+                <TableRow key={index}>
+                  <TableCell className='font-medium w-[40%]'>
+                    {question.question}
+                  </TableCell>
+                  <TableCell className='font-medium w-[35%]'>
+                    {question.options.map((option, index) => (
+                      <p key={index} className='leading-4 py-0.5'>
+                        {`${index + 1}. ${option},`}
+                      </p>
+                    ))}
+                  </TableCell>
+                  <TableCell className='font-medium'>
+                    <p>{question.subject.title}</p>
+                    <Badge className='italic shadow-none'>
+                      Answer: {Number(question.answer)}
+                    </Badge>
+                  </TableCell>
+                  <TableCell className='font-medium  space-y-3 flex flex-col items-start'>
+                    <Button
+                      size='sm'
+                      onClick={() => handleOpenModal(question, false)}
+                    >
+                      <PenBox size={18} className='mr-1' /> Edit
+                    </Button>
+                    <Button
+                      size='sm'
+                      variant='destructive'
+                      onClick={() => handleOpenModal(question.id, true)}
+                    >
+                      <Trash size={18} className='mr-1' /> Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
       </CardContent>
       {isOpen === true && (
         <ModalContainer
